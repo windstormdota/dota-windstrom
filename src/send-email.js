020d97584b6462af13d30f6816c15183c6283c90
Robin Granger <rg.robingranger@gmail.com>
2019-03-09 14:04:00 +0100

    correction(send-email) : utilise la globale window

--- src/send-email.js
+++ src/send-email.js
@@ -1,5 +1,5 @@
 export function sendEmail(data = {}) {
-	return fetch(`http://${location.host}/api/send-email`, {
+	return fetch(`http://${window.location.host}/api/send-email`, {
 		method: 'POST',
 		mode: 'cors',
 		headers: {
