ac3b19dfce75068d72e4de4b3b161630dcc5901a
Robin Granger <rg.robingranger@gmail.com>
2019-03-09 14:39:25 +0100

    evolution(email) : suite implementation server

--- server.js
+++ server.js
@@ -3,23 +3,50 @@ const polka = require('polka')
 const { join } = require('path')
 const serveStatic = require('serve-static')
 const { json } = require('body-parser')
+const nodemailer = require('nodemailer')
 
 const { PORT = 80 } = process.env
 const buildDir = join(__dirname, 'build')
 const serve = serveStatic(buildDir)
 
-polka()
-	.use(serve)
-	.use(json())
-	.post('/api/send-email', (req, res) => {
-		res.writeHead(200, { 'Content-Type': 'application/json' });
-		let json = JSON.stringify(req.body);
-		res.end(json);
+async function main () {
+	const account = await nodemailer.createTestAccount()
+	const transporter = nodemailer.createTransport({
+		host: "smtp.ethereal.email",
+		port: 587,
+		secure: false,
+		auth: {
+			user: account.user,
+			pass: account.pass,
+		}
 	})
-	.listen(PORT, (err) => {
-		if (err) throw err
-		console.log(`> Running on localhost:${PORT}`)
-	})
+
+	polka()
+		.use(serve)
+		.use(json())
+		.post('/api/send-email', (req, res) => {
+			try {
+				const info = await transporter.sendMail({
+					from: '"Toto email <toto@toto.toto>',
+					to: 'q299710@nwytg.net',
+					subject: 'Test: subject',
+					text: 'coucou this is text body',
+					html: '<h1> BOUZAAAA </h1>',
+				})
+				res.writeHead(200);
+				res.end()
+			} catch (err) {
+				res.writeHead(500)
+				res.end(err.message)
+			}
+		})
+		.listen(PORT, (err) => {
+			if (err) throw err
+			console.log(`> Running on localhost:${PORT}`)
+		})	
+}
+
+main().catch(console.error)
 
 // Prevents heroku from "sleeping"
 setInterval(() => {
--- src/App.js
+++ src/App.js
@@ -54,7 +54,7 @@ ${description}
     sendEmail({ type, gameID, playerName, heroes, minReplay, description })
     .then((data) => console.log('data', data))
     .catch((err) => console.log('error', err))
-  }, [])
+  }, [type, gameID, playerName, heroes, minReplay, description])
 
   return (
       <Container className="App-container align-items-center justify-content-center">
